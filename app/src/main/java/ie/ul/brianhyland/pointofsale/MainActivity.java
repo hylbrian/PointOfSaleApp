13a340f15b0428d3d5f1ab78eae2f04574db1d29
Brian Hyland <39333309+hylbrian@users.noreply.github.com>
2018-10-12 15:33:40 +0100

    long press

--- app/src/main/java/ie/ul/brianhyland/pointofsale/MainActivity.java
+++ app/src/main/java/ie/ul/brianhyland/pointofsale/MainActivity.java
@@ -10,12 +10,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
@@ -36,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
         mNameTextView = findViewById(R.id.name_text);
         mQuantityTextView = findViewById(R.id.quantity_text);
         mDateTextView = findViewById(R.id.date_text);
+
+        unregisterForContextMenu(mNameTextView);
 
         mItems = new ArrayList<>();
 
@@ -113,6 +117,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.menu_context, menu);
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_edit:
+                Toast.makeText(this,"TODO Edit", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_remove:
+                Toast.makeText(this,"TODO Remove", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // TODO: later
 
@@ -163,7 +188,7 @@ public class MainActivity extends AppCompatActivity {
 
         builder.setPositiveButton(android.R.string.ok, null);{
             mItems.clear();
-            
+
 
             }
 
